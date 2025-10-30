5d1d21f498e70114d607c67ba63a6cf668e5b7b1
mahnafdev <mahnafdev@gmail.com>
2025-10-30 09:59:13 +0600

    gsap: version

--- src/Index.tsx
+++ src/Index.tsx
@@ -1,3 +1,7 @@
+import gsap from "gsap";
+
 export default function Home() {
+	// GSAP Version
+	console.log(`GSAP ${gsap.version}`);
 	return <main className="p-24 flex flex-col gap-y-64"></main>;
 }
